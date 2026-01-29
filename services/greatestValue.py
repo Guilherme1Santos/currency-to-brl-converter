45e8f893d3fc5798385d606b2bc5d65f4d5e1a5d
Guilherme Santos <turnofff8@gmail.com>
2026-01-29 15:16:11 -0300

    update gratestvalue.py

--- services/greatestValue.py
+++ services/greatestValue.py
@@ -1,9 +1,8 @@
 import requests
 #will return the current value, and the highest value reached in the last 24 hours
-
 def searchCurrency(currency):
-    url = f'https://economia.awesomeapi.com.br/last/{currency}-BRL'
-    req = requests.get(url)
+    URL = f'https://economia.awesomeapi.com.br/last/{currency}-BRL'
+    req = requests.get(URL)
     if req.status_code == 200:
         data = req.json()[f"{currency}BRL"]
         return {
